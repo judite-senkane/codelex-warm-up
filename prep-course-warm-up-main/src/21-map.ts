68ca8afd444e35f2a01ac9a0ee6b62cf7d63d58a
Judite Senkane <judite.senkane@gmail.xom>
2023-02-10 20:24:57 +0200

    Exercise 31, updated previous exercises

--- prep-course-warm-up-main/src/21-map.ts
+++ prep-course-warm-up-main/src/21-map.ts
@@ -2,31 +2,22 @@ import { forEachChild } from 'typescript';
 
 export {};
 
-function tidyUpString(str: string[]) {
-  let tidyString = str.map((element) => {
-    return element.trim();
-  });
-  tidyString = tidyString.map((element) => {
-    return element.toLowerCase();
-  });
-  tidyString = tidyString.map((element) => {
-    return element.replace('/', '');
-  });
-  return tidyString;
+function tidyUpString(str: string): string {
+  return str.trim().toLowerCase().replace('/', '');
 }
 
 // You are allowed to edit this function
-function capitalise(str: string[]) {
-  let capitalString = str.map(element => {
-    const restOfString = element.slice(1);
-    const firstLetter = element.charAt(0).toUpperCase();
-    return firstLetter + restOfString;
-  })
-  return capitalString;
+function capitalise(str: string): string {
+  const restOfString = str.slice(1);
+  const firstLetter = str.charAt(0).toUpperCase();
+  return firstLetter + restOfString;
 }
 
 const mentors = ['/Daniel ', 'irina ', ' Gordon', 'ashleigh '];
-let mentorsTidy = tidyUpString(mentors); // You are allowed to edit this line
-mentorsTidy = capitalise(mentorsTidy);
-
+let mentorsTidy: string[] = []; // You are allowed to edit this line
+mentors.map((element) => {
+  tidyUpString(element);
+  capitalise(element);
+  mentorsTidy.push(element);
+});
 console.log(mentorsTidy); // Expected output: ["Daniel", "Irina", "Gordon", "Ashleigh"]
